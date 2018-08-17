9d39b969eccfcd666bcb7286536e51da38d746c6
Dustin Schau <dustinschau@gmail.com>
2018-08-17 13:35:22 -0600

    chore: show emotion

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
+import styled from 'react-emotion';
 
 import Header from './header';
 import 'normalize.css';
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1rem 1.5rem;
+  padding-top: 0;
+`;
 
 export default function Layout({ children, data, meta }) {
   return (
@@ -34,16 +42,7 @@ export default function Layout({ children, data, meta }) {
             <html lang="en" />
           </Helmet>
           <Header siteTitle={data.site.siteMetadata.title} />
-          <div
-            style={{
-              margin: '0 auto',
-              maxWidth: 960,
-              padding: '0px 1.0875rem 1.45rem',
-              paddingTop: 0
-            }}
-          >
-            {children}
-          </div>
+          <Container>{children}</Container>
         </>
       )}
     />
