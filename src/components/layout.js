5b74d1b8cd2802b94b76a041b2ad7e355307f2a9
Dustin Schau <dustinschau@gmail.com>
2018-08-17 13:31:22 -0600

    ore: add meta tag stuff

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,46 +6,54 @@ import { StaticQuery, graphql } from 'gatsby';
 import Header from './header';
 import 'normalize.css';
 
-const Layout = ({ children, data }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+export default function Layout({ children, data, meta }) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' }
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
-);
+      `}
+      render={data => (
+        <>
+          <Helmet
+            title="Blog"
+            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+            meta={[
+              {
+                name: 'description',
+                content: 'The newest of my (infrequent) blog posts.'
+              },
+              { name: 'keywords', content: 'javascript, react, angular, vue' }
+            ].concat(meta)}
+          >
+            <html lang="en" />
+          </Helmet>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: '0px 1.0875rem 1.45rem',
+              paddingTop: 0
+            }}
+          >
+            {children}
+          </div>
+        </>
+      )}
+    />
+  );
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
 };
 
-export default Layout;
+Layout.defaultProps = {
+  meta: []
+};
